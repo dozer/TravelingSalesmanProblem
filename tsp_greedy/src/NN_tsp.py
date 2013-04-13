433880c1a776e7e02d0085c515bd350e875e4578
Scott Stevenson <oodabaga@gmail.com>
2013-04-13 16:44:37 -0400

    Fixed error in NN algorithm implementation.
    - Fixed implementation errors in NN algorithm; now functions as
    intended.
    - Cleaned up parsing of cities from file
    - Added parsing of int and float coordinates
    - Added optimum tours chart

--- tsp_greedy/src/NN_tsp.py
+++ tsp_greedy/src/NN_tsp.py
@@ -3,7 +3,7 @@
 #Traveling Salesman Solution using NearestNeighbor Algorithm
 #Scott Stevenson, Jack Koppenaal, John Costantino
 
-import time, itertools, math
+import time, itertools, math, exceptions
 
 #Get the city file
 def getFile():
@@ -19,21 +19,25 @@ def getFile():
 def getDistance(city1, city2):
 	return 	math.sqrt((int(city2[1]) - int(city1[1]))**2 + (int(city2[2])-int(city1[2]))**2)
 
+def num(s):
+	try:
+		return int(s)
+	except exceptions.ValueError:
+		return float(s)
+
 #Get the cities from a specified city file
 def getCities(cityFile):
 	for line in cityFile:
 		try:
-			#Lines split by a space char will look like:
+			#Lines split by white space will look like:
 			#[ident, x-coord, y-coord]
-			ident = line.split(' ')[0]
-			x = line.split(' ')[1]
-			y = line.split(' ')[2].strip('\n')
-			#If the ident is not an int (not a city) skip it otherwise add it	
-			if ident.isdigit():
+			parts = line.split()
+			if parts[0].isdigit():
 				#Cities are just lists with values (almost a pseudo-class)
-				city = [int(ident), float(x), float(y), False]
+				#city = [num(ident), num(x), num(y), False]
+				city = [num(parts[0]), num(parts[1]), num(parts[2].strip('\n')), False]
 				cities.append(city)
-		except:
+		except Exception as e:
 			#The ident was not an int so we skip (pass) it and move on to the next
 			pass
 
@@ -42,13 +46,15 @@ def getNearestNeighbor(i):
 	#Start at infinity
 	nearest = [-1, float("inf")]
 
+	#print '\n cities:' + str(cities)
 	for index in range(len(cities)):
 		#Skip if looking at itself or looking at a visited node
-		if i == index or cities[index][3]:
+		if i == index or cities[index][3] == True:
 			pass
-		#Otherwise calculate distance and update nearest
+		#Otherwise calculate distance and update nearest if necessary
 		else:
 			distance = getDistance(cities[i], cities[index])
+			#print nearest
 			if distance < nearest[1]:
 				nearest = [index, distance]
 
@@ -60,9 +66,15 @@ def getNearestNeighbor(i):
 def nearestNeighbor(cities):
 	data = []
 	tour = [[],0]
+	first = getNearestNeighbor(0)
+	data.append(first)
+	next = first
 	#Get the nearest neighbor and weight for every vertex and append to the list
-	for index in range(len(cities)):
-		data.append(getNearestNeighbor(index))
+	for index in range(1,len(cities)):
+		cur = getNearestNeighbor(next[0])
+		data.append(cur)
+		next = cur
+
 	#Once we have the path and weight, split into 2 arrays for ease of use
 	for element in data:
 		tour[1] += element[1]
@@ -107,9 +119,9 @@ def toFile(tour):
 #-------------------The actual script begins here-----------------------
 cities = []
 cityFile = getFile()
+getCities(cityFile)
 #Start the stopwatch
 start = time.time()
-getCities(cityFile)
 opt_tour = nearestNeighbor(cities)
 #Stop the stopwatch
 finish = time.time()
